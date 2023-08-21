8549646a8d436f8cb42c4f7559f0a8e581acf50b
Pankaj Ganjale <pankajganjale2805@gmail.com>
2023-08-21 21:55:20 +0530

    nested html in react

--- core/App.js
+++ core/App.js
@@ -1,8 +1,22 @@
-const heading = React.createElement('h1', { id: "heading" }, "This is h1 tag.");
+const heading = React.createElement('h1', {}, 'This is h1 tag.');
 
-const child = React.createElement('div', { id: "child" }, heading);
+const child = React.createElement(
+  'div',
+  { id: 'child' },
+  [
+    { ...heading, key: 'heading1', props: { ...heading.props, class: 'heading1' } },
+    { ...heading, key: 'heading2', props: { ...heading.props, class: 'heading2' } },
+  ],
+);
 
-const parent = React.createElement('div', { id: "parent" }, child);
+const parent = React.createElement(
+  'div',
+  { id: 'parent' },
+  [
+    { ...child, key: 'child1', props: { ...child.props, id: 'child1' } },
+    { ...child, key: 'child2', props: { ...child.props, id: 'child2' } },
+  ],
+);
 
 const rootElement = document.getElementById('root');
 
